428449b285c402d088d71aaf6cfdc7ada1eade60
cxlos <bautistac_29@outlook.com>
2022-11-17 15:34:36 -0600

    got some help in tutoring session.

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,14 @@
 ///////////////Timer variables///////////////////
-var timer = document.querySelector('#timer')
+const timer = document.querySelector('#timer')
+const quizbox = document.querySelector('#quizbox')
 const question = document.querySelector('#question')
 const ready = document.querySelector('.ready')
-const randomQ, currentQIndex
+const answer = document.querySelector('.btnbox')
+var x = 60
+let randomQ, currentQIndex
+
 ////////////////Timer/////////////////////
 function countDown()  {
-    var x = 60
     var timeInterval = setInterval(function () {
 
         if(x > 0) {
@@ -17,7 +20,7 @@ function countDown()  {
             timer.textContent = 'Times Up!';
             clearInterval(timeInterval);
             
-            displayMessage();
+            gameOver();
         }
 
         console.log('started')
@@ -25,29 +28,79 @@ function countDown()  {
 }
 
 /////////////////Display Message//////////////////
-function displayMessage () {
-    var count = 0;
+function gameOver () {
+    // var over = document.createElement('h2')
+    // over.classList.add('h2')
+    question.innerHTML = 'Game Over!'
+    answer.classList.add('hide')
 
-    var msgInterval = setInterval(function () {
-        if (words[count] === undefined) {
-            clearInterval(msgInterval);
+//     var count = 0;
 
-        } else {
-            timer.textContent = words[count];
-            count ++;
-        }
-    }, 1000)
-}
+//     var msgInterval = setInterval(function () {
+//         if (word[count] === undefined) {
+//             clearInterval(msgInterval);
+
+//         } else {
+//             timer.textContent = words[count];
+//             count ++;
+//         }
+//     }, 1000)
+ }
 //////////////////////Start Quiz/////////////////////
 function letsGo() {
     startq.classList.add('hide')
     ready.classList.add('hide')
+    answer.classList.remove('hide')
     question.classList.remove('hide')
-    randomQ = question0.sort(() => Math.random() - .5)
+    randomQ = quizz.sort(() => Math.random() - .5)
     currentQIndex = 0
-    nextQuestion()
+    nextQ()
 }
 ///////////////////Quiz Questions///////////////////
+
+
+function nextQ() {
+    showQ(randomQ[currentQIndex])
+}
+
+function showQ(question0){
+    question.innerText = question0.question0
+    answer.innerHTML = ''
+    question0.answers.forEach(option => {
+        const button = document.createElement('button')
+        button.innerText = option.text
+        button.classList.add('btn')
+        if (option.correct) {
+            button.dataset.correct = option.correct
+        }
+
+        console.log(option)
+        button.addEventListener('click', chooseA)
+        answer.appendChild(button)
+        
+    });
+}
+
+// function restartQ() {
+//     nextb
+// }
+
+function chooseA(e) {
+    e.preventDefault() 
+    console.log(e) 
+    var isCorrect = e.target.getAttribute ('data-correct')
+
+    if (isCorrect) {
+        currentQIndex ++
+        nextQ()       
+        // alert('Correct!')
+    } else {
+        // alert('Incorrect')
+        x -= 10
+    }
+    
+}
+
 const quizz = [
     {
         question0: 'What was the start date of this class?',
@@ -56,15 +109,36 @@ const quizz = [
             {text: 'Oct. 18th', correct: false},
             {text: 'Oct. 17th', correct: true},
             {text: 'Oct. 20th', correct: false},
-          
+        ]
+    },
+    {
+        question0: 'What topic did we cover in week 4 of the bootcamp?',
+        answers: [
+            {text: '', correct: false },
+            {text: 'Web APIs', correct: true },
+            {text: 'Javascript', correct: false },
+            {text: 'CSS', correct: false },
+        ]
+    },
+    {
+        question0: 'Which of the following is a vegetable?',
+        answers: [
+            {text: 'Tomato', correct: false },
+            {text: 'Cucumber', correct: false },
+            {text: 'Pumpkin', correct: false },
+            {text: 'Corn', correct: true },
+        ]
+    },
+    {
+        question0: 'Which continent is Kazakstan in?',
+        answers: [
+            {text: 'Europe', correct: false },
+            {text: 'Asia', correct: true },
+            {text: 'Antarctica', correct: false },
+            {text: 'Africa', correct: false },
         ]
     }
 ]
-
-function nextQ() {
-    showQ(randomQ[currentQIndex])
-
-}
 
 ///////////////////eventListener//////////////////
 var startq = document.querySelector('#go');
@@ -73,3 +147,10 @@ startq.addEventListener('click', letsGo);
 startq.addEventListener('click', countDown);
 
 
+/////////Tutoring Questions////////////
+
+//1. function (element) ??
+//2. function (element, index) ??
+//3. cant append new buttons and make old ones disappear
+//4. getting error 'word not defined at ln 34'
+//5. 
